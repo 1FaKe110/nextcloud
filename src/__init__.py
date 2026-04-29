ab1a609a30bbd96ea8ef4eeba09fddf09fb1ee10
ifake <gabko2016@gmail.com>
2026-04-29 14:54:46 +0300

    fix imports

--- src/__init__.py
+++ src/__init__.py
@@ -8,5 +8,18 @@ from nextcloud.bot.core.models import (
 )
 
 
+# Экспортируем все классы
 __version__ = "0.2.0"
-__all__ = ["Bot", "Update", "Message", "User", "Chat", "Audio", "Video", "Document", "Photo"]
+__all__ = [
+    "Bot",           # синхронная версия (по умолчанию)
+    "AsyncBot",      # асинхронная версия (явно)
+    "Update",
+    "Message",
+    "User",
+    "Chat",
+    "File",
+    "Audio",
+    "Video",
+    "Document",
+    "Photo"
+]
